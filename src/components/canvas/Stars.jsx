d2ada17b83a5c7a4c0c840a0a8d5da3d5aabf9aa
naveen076960 <naveenoshada15@gmail.com>
2026-03-12 02:06:15 +0530

    final update

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Stars = () => {
-  return (
-    <div>Stars</div>
-  )
-}
+  return null;
+};
 
-export default Stars
+export default Stars;
