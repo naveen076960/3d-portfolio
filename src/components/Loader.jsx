53397b5e9f89d43c66ae275486051ae3f756c8b3
naveen076960 <naveenoshada15@gmail.com>
2026-03-12 01:56:27 +0530

    Fixed R3F Div crash with proper Html and mesh tags

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Html } from "@react-three/drei";
 
 const Loader = () => {
   return (
-    <div>Loader</div>
-  )
-}
+    <Html as="div" center>
+      <div className="flex flex-col items-center justify-center">
+        <span className="w-8 h-8 border-4 border-[#00f7ff] border-t-transparent rounded-full animate-spin"></span>
+        <p className="text-[#00f7ff] font-mono text-sm mt-4 font-bold tracking-widest">
+          LOADING_ASSETS...
+        </p>
+      </div>
+    </Html>
+  );
+};
 
-export default Loader
+export default Loader;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,4 +1,14 @@
-const Earth = () => {
-  return <div>ComputerCanvas</div>;
+import React from "react";
+
+const ComputersCanvas = () => {
+  return (
+    // A <mesh> is a basic 3D object. This creates a cool wireframe box
+    // so your site doesn't crash while you build the real models!
+    <mesh>
+      <boxGeometry args={[2, 2, 2]} />
+      <meshStandardMaterial color="#00f7ff" wireframe={true} />
+    </mesh>
+  );
 };
+
 export default ComputersCanvas;
